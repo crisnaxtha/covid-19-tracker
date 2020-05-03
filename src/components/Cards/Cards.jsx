f22d88293d025924c53390d024a435dc6ebe6a5e
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-05-03 12:05:31 +0545

    Worked on Card and it is completed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -26,7 +26,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
     return (
         <div className={ styles.container }>
             <Grid container spacing={3} justify="center">
-                <Grid item component={ Card } className={ cx(styles.card, styles.infected)}>
+                <Grid item component={ Card } xs={12} md={3} className={ cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -41,7 +41,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
                         <Typography variant="body2">Number of active of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={ Card }>
+                <Grid item component={ Card }  xs={12} md={3} className={ cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -55,7 +55,7 @@ const Cards = ({data: { confirmed, recovered, deaths, lastUpdate} }) => {
                         <Typography variant="body2">Number of recovered of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={ Card }>
+                <Grid item component={ Card }  xs={12} md={3} className={ cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
